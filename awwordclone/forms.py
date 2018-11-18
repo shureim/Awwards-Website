8873b8f2363b568b06c235e68b25b8b25febc963
shureim <tarickaliabdi@gmail.com>
2018-11-18 15:21:49 +0300

    added projectform for users to add project

--- awwordclone/forms.py
+++ awwordclone/forms.py
@@ -1,11 +1,11 @@
-# from django import forms
-# from .models import Project,Profile
-#
-# class ProjectForm(forms.ModelForm):
-#     class Meta:
-#         model = Project
-#         exclude = ['profile']
-#
+from django import forms
+from .models import Project,Profile
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['profile']
+
 # class ProfileForm(forms.ModelForm):
 #     class Meta:
 #         model = Profile
