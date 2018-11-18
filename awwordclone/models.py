3235023a04ab1edb34eca2c783426c51846dcf86
shureim <tarickaliabdi@gmail.com>
2018-11-18 15:16:33 +0300

    added delete_profile in the  profile class in  the models.py

--- awwordclone/models.py
+++ awwordclone/models.py
@@ -40,6 +40,6 @@ class Profile(models.Model):
 
     def save_profile(self):
         self.save()
-#
-#     def delete_profile(self):
-#         self.delete()
+
+    def delete_profile(self):
+        self.delete()
