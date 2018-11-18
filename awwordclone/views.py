7d713f98da91424b7291e77fbc45022b62df0fb8
shureim <tarickaliabdi@gmail.com>
2018-11-18 15:06:38 +0300

    added project function in the views.py

--- awwordclone/views.py
+++ awwordclone/views.py
@@ -11,13 +11,13 @@ def index(request):
     current_user = request.user
     projects = Project.get_all()
     return render(request,'index.html',{'projects':projects})
-#
-# def project(request,project_id):
-#     try:
-#         project = Project.objects.get(id = project_id)
-#     except ObjectDoesNotExist:
-#         raise Http404()
-#     return render(request,'project-detail.html',{'project':project})
+
+def project(request,project_id):
+    try:
+        project = Project.objects.get(id = project_id)
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request,'project-detail.html',{'project':project})
 #
 # @login_required(login_url='/accounts/login/')
 # def new_project(request):
