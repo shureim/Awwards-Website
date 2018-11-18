7f39fc22274a45d6c6e4dace388ef49342bf9a37
shureim <tarickaliabdi@gmail.com>
2018-11-18 15:09:43 +0300

    added url for new_project in the urls.py

--- awwordclone/urls.py
+++ awwordclone/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.index,name='indexPage'),
     url(r'^projects/(\d+)',views.project,name ='projects'),
-    # url(r'^new/project$', views.new_project, name='new-project'),
+    url(r'^new/project$', views.new_project, name='new-project'),
     # url(r'^profile/',views.profile,name = 'Profile'),
     # url(r'^create-profile/$',views.create_profile,name = 'create-profile'),
     # url(r'^search/', views.search_results, name='search_results'),
