ebea8f352782a7f34237735b295d193e3afd4a14
shureim <tarickaliabdi@gmail.com>
2018-11-18 15:10:25 +0300

    added url for create_profile in the urls.py

--- awwordclone/urls.py
+++ awwordclone/urls.py
@@ -8,7 +8,7 @@ urlpatterns=[
     url(r'^projects/(\d+)',views.project,name ='projects'),
     url(r'^new/project$', views.new_project, name='new-project'),
     url(r'^profile/',views.profile,name = 'Profile'),
-    # url(r'^create-profile/$',views.create_profile,name = 'create-profile'),
+    url(r'^create-profile/$',views.create_profile,name = 'create-profile'),
     # url(r'^search/', views.search_results, name='search_results'),
     # url(r'^project-detail/(\d+)',views.search_project,name = 'project-detail'),
 
